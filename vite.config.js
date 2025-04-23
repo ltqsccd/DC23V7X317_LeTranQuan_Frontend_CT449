6c12d4572a4cd446ec5d0b9c7ad980fe3612809a
DC23V7X317_LeTranQuan <ltqsccd@gmail.com>
2025-04-23 12:57:53 +0700

    Hien thi danh sach cac contact lay ve tu server

--- vite.config.js
+++ vite.config.js
@@ -2,20 +2,25 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+// import { Server } from 'node:http' ← Dòng này không cần thiết, có thể xóa
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
+    }
   },
   server: {
     port: 3001,
-    },
+  proxy: {
+    "/api": {
+      target: "http://localhost:3000/",
+      changeOrigin: true
+    }
+  }
+  }
 })
